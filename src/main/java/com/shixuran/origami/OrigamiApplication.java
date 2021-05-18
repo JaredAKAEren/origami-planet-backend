4b023967d73c379f1f2eefcbfde9f352bb5aa40d
JaredAKAEren <shixuran@qq.com>
2021-05-18 17:41:23 +0800

    initial
    初始化Spring Boot项目

---
+++ src/main/java/com/shixuran/origami/OrigamiApplication.java
@@ -0,0 +1,13 @@
+package com.shixuran.origami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrigamiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrigamiApplication.class, args);
+    }
+
+}
